e7c5652ccf2b64d1e5bb990d1549adfe6c16cb66
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-05-22 15:20:16 -0400

    Add user_age input and calculate user age

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What\'s your name?"
-user_name = gets
-puts "Hello, #{user_name}"
+user_name = gets.chomp
+puts "Hello, #{user_name}."
+puts "How old are you?"
+user_age = gets.to_i
+puts "You were born in #{2018 - user_age}"
